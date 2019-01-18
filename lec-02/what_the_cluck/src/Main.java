ec4aa97b7c530f86d6df1351b16ff4691116ea0c
Jordan Pratt <jpratt@mtroyal.ca>
2019-01-17 17:26:02 -0700

    Update Main.java

--- lec-02/what_the_cluck/src/Main.java
+++ lec-02/what_the_cluck/src/Main.java
@@ -11,7 +11,19 @@ public class Main {
 
 	int numHens = keyboard.nextInt();
 
-	System.out.println("the num of hens is : " + numHens);
+	WhatTheCluckApp app = new WhatTheCluckApp();
+
+	int lowestNumEggs = app.lowestNumEggs(numHens);
+	int highestNumEggs = app.highestNumEggs(numHens);
+
+	System.out.printf("%,d hens will likely produce between %,d and %,d eggs.%n",
+			  numHens, lowestNumEggs, highestNumEggs);
+
+	int lowestNumCartons = app.lowestNumCartons(lowestNumEggs);
+	int highestNumCartons = app.highestNumCartons(highestNumEggs);
+
+	System.out.printf("This will require between %,d and %,d cartons.%n",
+			  lowestNumCartons, highestNumCartons);
 
     }
 
